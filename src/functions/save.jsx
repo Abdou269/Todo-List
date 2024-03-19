7ea31745e0d10d757c77c4674fc3a3f2526e4c84
Abdou269 <abdrrahim.711@gmail.com>
2024-03-19 16:52:50 +0100

    new

--- src/functions/save.jsx
+++
@@ -1,6 +0,0 @@
-export default function save(array, state, dispatch){
-    const oldStorage = JSON.parse(localStorage.getItem('lists'));
-    const newStorage = oldStorage.map(list => list.title === state.listTitle ? {...list, tasks: array} : list);
-    localStorage.setItem('lists', JSON.stringify(newStorage));
-    dispatch({type: 'showList', value: true});
-}
